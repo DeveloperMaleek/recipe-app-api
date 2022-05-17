e890529e877e22daecf8ab8e16b5b7ae1177723e
Malik Kolade <kolademalikademola@gmail.com>
2022-05-17 08:08:49 +0000

    postgresql downloaded and setup

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+calculator function
+"""
+
+
+def add(x, y):
+    """Adds x and y and return result."""
+    return x + y
+
+
+def substract(x, y):
+    """Subtract x and y and return result."""
+    return x - y
